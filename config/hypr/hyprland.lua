432a5d8804baa7f2eb71c7972c9c36239d57c0db
Avdhut-code <useonlyforstudy01@gmail.com>
2026-07-01 02:33:26 +0530

    small fix

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -109,12 +109,10 @@ hl.config({
 -- new curve/animation API.
 hl.curve("myBezier", { type = "bezier", points = { {0.05, 0.9}, {0.1, 1.05} } })
 
-hl.animation({ leaf = "windows",    enabled = true, speed = 2, bezier = "myBezier" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 2, bezier = "default", style = "popin 80%" })
-hl.animation({ leaf = "border",     enabled = true, speed = 2, bezier = "default" })
-hl.animation({ leaf = "borderangle",enabled = true, speed = 2,  bezier = "default" })
-hl.animation({ leaf = "fade",       enabled = true, speed = 2,  bezier = "default" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 2,  bezier = "default" })
+hl.animation({ leaf = "windows",    enabled = true, speed = 1, bezier = "myBezier" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 1, bezier = "default", style = "popin 80%" })
+hl.animation({ leaf = "fade",       enabled = true, speed = 1,  bezier = "default" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 1,  bezier = "default" })
 
 
 -------------------
